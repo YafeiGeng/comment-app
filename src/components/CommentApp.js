87b0629630dcfa5b32483355c2b25af0c871bdc2
YafeiQQ <yafeigeng1228@gmail.com>
2019-07-23 12:16:07 -0700

    refacter CommentApp component

--- src/components/CommentApp.js
+++ src/components/CommentApp.js
@@ -5,14 +5,28 @@ import CommentList from './CommentList';
 
 class CommentApp extends React.Component{
 
-    handleSubmitComment=(comment)=>{
-        console.log(comment);
+    constructor() {
+        super();
+        this.state={
+            comments:[]
+        }
     }
+
+    handleSubmitComment=(comment)=>{
+        if(!comment) return;
+        if(!comment.username) return alert('Enter username, please');
+        if(!comment.content) return alert('Enter content, please');
+        this.state.comments.push(comment);
+        this.setState({
+            comments:this.state.comments
+        })
+    };
+
     render(){
         return (
             <div className='wrapper'>
                 <CommentInput onSubmit={this.handleSubmitComment}/>
-                <CommentList />
+                <CommentList comments={this.state.comments}/>
             </div>
         )
     }
