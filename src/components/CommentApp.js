4ea59f79360c6d4e626d3c0ac8694b29fa643990
YafeiQQ <yafeigeng1228@gmail.com>
2019-07-23 11:48:53 -0700

    edit CommentApp component

---
+++ src/components/CommentApp.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
+
+
+class CommentApp extends React.Component{
+
+    handleSubmitComment=(comment)=>{
+        console.log(comment);
+    }
+    render(){
+        return (
+            <div className='wrapper'>
+                <CommentInput onSubmit={this.handleSubmitComment}/>
+                <CommentList />
+            </div>
+        )
+    }
+}
+
+export default CommentApp;
